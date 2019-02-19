ce6a235adf79ee195c16b0dd9f3d65418abb6e2c
Chylli <chylli@binary.com>
2019-02-19 15:31:37 +0800

    another version

--- src/main.rs
+++ src/main.rs
@@ -37,10 +37,10 @@ fn tokenize(s: &str) -> Vec<Token> {
                     ')' => tokens.push(Token::CloseParen),
                     '+' => tokens.push(Token::Identifier(c.to_string())),
                     '0'... '9' => {
-                        let mut extra_chars: Vec<char> = iter.by_ref().take_while(|cc| cc.is_digit(10)).collect();
-                        let mut chars = vec![c];
-                        chars.append(&mut extra_chars);
-                        let val = chars.into_iter().collect::<String>().parse::<i32>().unwrap();
+                        let extra_chars: String = iter.by_ref().take_while(|cc| cc.is_digit(10)).collect();
+                        let mut chars = c.to_string();
+                        chars.push_str(&extra_chars);
+                        let val = chars.parse::<i32>().unwrap();
 
                         tokens.push(Token::Integer(val))
                     },
