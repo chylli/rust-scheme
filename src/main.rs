731bd09a2103cf347ae40202d777e24d4cf7d7b0
Chylli <chylli@binary.com>
2019-02-19 09:44:09 +0800

    First pass at tokenizer. d425e6aed7984362e34d29f6ebfb3c849dfbfd6a

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
 fn main() {
-    println!("hello?");
+    run("(+ 2 3)");
+}
+
+fn run(s: &str) {
+    println!("str: \"{}\"", s);
+    let tokens = tokenize(s);
+    println!("tokens: {:?}", tokens);
+}
+
+enum Token {
+    OpenParen,
+    CloseParen,
+    Identifier(String),
+    Integer(u32),
+}
+
+impl std::fmt::Debug for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Token::OpenParen => write!(f, "OpenParen"),
+            Token::CloseParen => write!(f, "CloseParen"),
+            Token::Identifier(ref v) => write!(f, "Identifier({})", v),
+            Token::Integer(ref v) => write!(f, "Integer({})", v),
+        }
+    }
+}
+
+fn tokenize(s: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    for c in s.chars() {
+        match c {
+            '(' => tokens.push(Token::OpenParen),
+            ')' => tokens.push(Token::CloseParen),
+            '+' => tokens.push(Token::Identifier(c.to_string())),
+            '0'...'9' => tokens.push(Token::Integer(c.to_digit(10).unwrap())),
+            ' ' => (),
+            _   => println!("other"),
+        };
+    }
+    tokens
 }
