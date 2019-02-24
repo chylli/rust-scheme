62d66d05b502dd3f3b4497bd0a50dab74e0be0a5
Chylli <chylli@binary.com>
2019-02-24 16:42:54 +0800

    7e1373c * Added interpreter evaluation context, definition of variables.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,9 +1,11 @@
 use crate::parser::Node;
 
 use std::fmt;
+use std::collections::HashMap;
 
 pub fn interpret(nodes: &Vec<Node>) -> Result<Vec<Value>, RuntimeError> {
-    evaluate_nodes(nodes)
+    let mut env = Environment::root();
+    evaluate_nodes(nodes, &mut env)
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -14,6 +16,7 @@ pub enum Value {
     List(Vec<Value>),
 }
 
+#[derive(Debug)]
 pub struct RuntimeError {
     message: String,
 }
@@ -30,32 +33,58 @@ macro_rules! runtime_error{
     )
 }
 
-fn evaluate_nodes(nodes: &Vec<Node>) -> Result<Vec<Value>, RuntimeError> {
+struct Environment {
+    values: HashMap<String, Value>,
+}
+
+impl Environment {
+    fn root() -> Environment {
+        Environment { values: HashMap::new()}
+    }
+
+    fn set(&mut self, key: String, value: Value) {
+        self.values.insert(key, value);
+    }
+
+    fn get(&self, key: &String) -> Option<Value> {
+        match self.values.get(key) {
+            Some(val) => Some(val.clone()),
+            None => None
+        }
+    }
+}
+
+fn evaluate_nodes(nodes: &Vec<Node>, env: &mut Environment) -> Result<Vec<Value>, RuntimeError> {
     let mut results = Vec::new();
     for node in nodes.iter() {
-        let res = evaluate_node(node)?;
+        let res = evaluate_node(node, env)?;
         results.push(res);
     };
     Ok(results)
 }
 
-fn evaluate_node(node: &Node) -> Result<Value, RuntimeError> {
+fn evaluate_node(node: &Node, env: &mut Environment) -> Result<Value, RuntimeError> {
     match node {
+        Node::Identifier(v) => {
+            match env.get(v) {
+                Some(val) => Ok(val),
+                None => runtime_error!("Identifier not found: {:?}", node)
+            }
+        }
         Node::Integer(v) => Ok(Value::Integer(*v)),
         Node::Boolean(v) => Ok(Value::Boolean(*v)),
         Node::String(v) => Ok(Value::String(v.clone())),
         Node::List(vec) => {
             if vec.len() > 0 {
-                evaluate_expression(vec)
+                evaluate_expression(vec, env)
             } else {
                 Ok(Value::List(Vec::new()))
             }
-        },
-        _ => runtime_error!("Can't evaluate node: {:?}", node)
+        }
     }
 }
 
-fn evaluate_expression(nodes: &Vec<Node>) -> Result<Value, RuntimeError> {
+fn evaluate_expression(nodes: &Vec<Node>, env: &mut Environment) -> Result<Value, RuntimeError> {
     let (first, others) = match nodes.split_first() {
         Some(v) => v,
         None => runtime_error!("Can't evaluate an empty expression: {:?}", nodes)
@@ -65,13 +94,24 @@ fn evaluate_expression(nodes: &Vec<Node>) -> Result<Value, RuntimeError> {
     match first {
         Node::Identifier(func) => {
             match func.as_ref() {
+                "define" => {
+                    if nodes.len() !=3 {
+                        runtime_error!("Must supply exactly two arguments to define: {:?}", nodes);
+                    }
+                    let name = match others.first().unwrap() {
+                        Node::Identifier(x) => x,
+                        _ => runtime_error!("Unexpected node for name in define {:?}", nodes)};
+                    let val = evaluate_node(others.last().unwrap(), env)?;
+                    env.set(name.clone(),val);
+                    Ok(Value::Integer(0)) // TODO change to more sensible return value
+                },
                 "+" => {
                     if nodes.len() < 2 {
                         runtime_error!("Must supply at least two arguments to +: {:?}", nodes);
                     }
                     let mut sum = 0;
                     for n in others.iter() { 
-                        let v = evaluate_node(n)?;
+                        let v = evaluate_node(n, env)?;
                         match v {
                             Value::Integer(x) => sum += x,
                             _ => runtime_error!("Unexpected node during +: {:?}", n)
@@ -83,8 +123,8 @@ fn evaluate_expression(nodes: &Vec<Node>) -> Result<Value, RuntimeError> {
                     if others.len() != 2 {
                         runtime_error!("Must supply exactly two arguments to -: {:?}", nodes);
                     }
-                    let v1 = evaluate_node(others.first().unwrap())?;
-                    let v2 = evaluate_node(others.last().unwrap())?;
+                    let v1 = evaluate_node(others.first().unwrap(), env)?;
+                    let v2 = evaluate_node(others.last().unwrap(), env)?;
                     let mut result = match v1 {
                         Value::Integer(x) => x,
                         _ => runtime_error!("Unexpected node during -: {:?}", nodes)
@@ -104,4 +144,10 @@ fn evaluate_expression(nodes: &Vec<Node>) -> Result<Value, RuntimeError> {
             runtime_error!("First element in an expression must be an identifier: {:?}", first);
         }
     }
+}
+
+#[test]
+fn test_global_variables() {
+    assert_eq!(interpret(&vec![Node::List(vec![Node::Identifier("define".to_string()), Node::Identifier("x".to_string()), Node::Integer(2)]), Node::List(vec![Node::Identifier("+".to_string()), Node::Identifier("x".to_string()), Node::Identifier("x".to_string()), Node::Identifier("x".to_string())])]).unwrap(),
+               vec![Value::Integer(0), Value::Integer(6)]);
 }
