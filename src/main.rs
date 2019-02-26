46abf31586793da8572241c1e381596914821cdd
Chylli <chylli@binary.com>
2019-02-26 22:18:10 +0800

    8385851 * Cleaned up main file with some macros.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ mod lexer;
 mod parser;
 mod interpreter;
 
+//TODO https://github.com/rust-lang/rfcs/blob/master/text/0243-trait-based-exception-handling.md  rewrite it to use exception type casting
+macro_rules! try_or_err_to_str{
+    ($inp:expr) => (
+        match $inp {
+            Ok(v) => v,
+            Err(e) => return Err(e.to_string())
+        })
+}
 fn main() {
     run("(+ 2 3)");
 }
@@ -12,90 +20,77 @@ fn run(input: &str) {
 }
 
 fn execute(input: &str) -> Result<String, String> {
-    let tokens = match lexer::tokenize(input) {
-        Ok(t) => t,
-        Err(e) => return Err(e.to_string())
-    };
-    println!("tokens: {:?}", tokens);
-
-    let ast = match parser::parse(&tokens) {
-        Ok(t) => t,
-        Err(e) => return Err(e.to_string())
-    };
-    println!("ast: {:?}", ast);
-
-    let result = match interpreter::interpret(&ast) {
-        Ok(t) => t,
-        Err(e) => return Err(e.to_string())
-    };
+    let tokens = try_or_err_to_str!(lexer::tokenize((input)));
+    let ast = try_or_err_to_str!(parser::parse(&tokens));
+    let result = try_or_err_to_str!(interpreter::interpret(&ast));
 
     Ok(format!("{}", result))
 }
 
+macro_rules! assert_execute{
+    ($src:expr, $res:expr) => (
+        assert_eq!(execute($src).unwrap().as_str(), $res)
+    )
+}
+
+macro_rules! assert_execute_fail{
+    ($src:expr, $res:expr) => (
+        assert_eq!(execute($src).err().unwrap().as_str(), $res)
+    )
+}
+
 #[test]
 fn test_basic_identities() {
-    assert_eq!(execute("1").unwrap().as_str(),
-               "1");
-    assert_eq!(execute("#f").unwrap().as_str(),
-               "#f");
-    assert_eq!(execute("\"hi\"").unwrap().as_str(),
-               "\"hi\"");
-    assert_eq!(execute("(lambda (x) x)").unwrap().as_str(),
-               "#<procedure>");
+    assert_execute!("1", "1");
+    assert_execute!("#f", "#f");
+    assert_execute!("\"hi\"", "\"hi\"");
+    assert_execute!("(lambda (x) x)", "#<procedure>");
 }
 
 #[test]
 fn test_simple_function() {
-    assert_eq!(execute("(+ 2 3)").unwrap().as_str(),
-               "5");
+    assert_execute!("(+ 2 3)", "5");
 }
 
 #[test]
 fn test_multiple_expression_return() {
-    assert_eq!(execute("(+ 2 3)\n(+ 1 2)").unwrap().as_str(),
-               "3");
+    assert_execute!("(+ 2 3)\n(+ 1 2)", "3");
 }
 
 #[test]
 fn test_nested_expressions() {
-    assert_eq!(execute("(+ 2 (- (+ 9 1) 4))").unwrap().as_str(),
-               "8");
+    assert_execute!("(+ 2 (- (+ 9 1) 4))", "8");
 }
 
 #[test]
 fn test_variable_definition() {
-    assert_eq!(execute("(define x 2) (+ x x x)").unwrap().as_str(),
-               "6");
+    assert_execute!("(define x 2) (+ x x x)", "6");
 }
 
 #[test]
 fn test_duplicate_variable_definition() {
-    assert_eq!(execute("(define x 2) (define x 3)").err().unwrap().as_str(),
-               "RuntimeError: Duplicate define: x");
+    assert_execute_fail!("(define x 2) (define x 3)", "RuntimeError: Duplicate define: x");
 }
 
 #[test]
 fn test_variable_modification() {
-    assert_eq!(execute("(define x 2) (set! x 3) (+ x x x)").unwrap().as_str(),
-               "9");
+    assert_execute!("(define x 2) (set! x 3) (+ x x x)", "9");
 }
 
 #[test]
 fn test_unknown_variable_modification() {
-    assert_eq!(execute("(set! x 3)").err().unwrap().as_str(),
-               "RuntimeError: Can't set! an undefined variable: x");
+    assert_execute_fail!("(set! x 3)", "RuntimeError: Can't set! an undefined variable: x");
 }
 
 #[test]
 fn test_procedure_definition() {
-    assert_eq!(execute("(define double (lambda (x) (+ x x))) (double 8)").unwrap().as_str(),
-               "16");
+    assert_execute!("(define double (lambda (x) (+ x x))) (double 8)", "16");
 }
 
 #[test]
 fn test_bad_syntax() {
-    assert_eq!(execute("(22+)").err().unwrap().as_str(),
+    assert_execute_fail!("(22+)",
                "SyntaxError: Unexpected character when looking for a delimiter: + (line: 1, column: 4)");
-    assert_eq!(execute("(+ 2 3)\n(+ 1 2-)").err().unwrap().as_str(),
+    assert_execute_fail!("(+ 2 3)\n(+ 1 2-)",
                "SyntaxError: Unexpected character when looking for a delimiter: - (line: 2, column: 7)");
 }
